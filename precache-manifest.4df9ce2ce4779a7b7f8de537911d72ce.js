11a2abba6a6e72d0282ec6990eb599e6885815b9
Vadim <zaytsewvk@gmail.com>
2019-03-17 15:04:05 +0200

    Updates

---
+++ precache-manifest.4df9ce2ce4779a7b7f8de537911d72ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10c5ba5c7c05bdba188",
+    "url": "/Zaytsewvk/Online-shop-react/static/css/main.58ea9b66.chunk.css"
+  },
+  {
+    "revision": "f10c5ba5c7c05bdba188",
+    "url": "/Zaytsewvk/Online-shop-react/static/js/main.f10c5ba5.chunk.js"
+  },
+  {
+    "revision": "1086d558957ccb7a2a5d",
+    "url": "/Zaytsewvk/Online-shop-react/static/js/runtime~main.1086d558.js"
+  },
+  {
+    "revision": "c1980cd844859446a5a5",
+    "url": "/Zaytsewvk/Online-shop-react/static/js/2.c1980cd8.chunk.js"
+  },
+  {
+    "revision": "a6412d5d4af4dbf1181ee478704b8332",
+    "url": "/Zaytsewvk/Online-shop-react/static/media/cash.a6412d5d.png"
+  },
+  {
+    "revision": "ee504c631230e326a742a2583ec2fe29",
+    "url": "/Zaytsewvk/Online-shop-react/static/media/card-payment.ee504c63.png"
+  },
+  {
+    "revision": "4387fe73eff7c1a0b3f87652b3b73d81",
+    "url": "/Zaytsewvk/Online-shop-react/static/media/NovaPoshta.4387fe73.png"
+  },
+  {
+    "revision": "30673c54b1d45771852e40cb485adc48",
+    "url": "/Zaytsewvk/Online-shop-react/static/media/courier.30673c54.png"
+  },
+  {
+    "revision": "25f9932584bbf5e427340a1f6abb7494",
+    "url": "/Zaytsewvk/Online-shop-react/static/media/ukrposhta.25f99325.jpg"
+  },
+  {
+    "revision": "d0772f5454af9da3685d1bcb72112b74",
+    "url": "/Zaytsewvk/Online-shop-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Zaytsewvk/Online-shop-react/precache-manifest.4df9ce2ce4779a7b7f8de537911d72ce.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Zaytsewvk/Online-shop-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
